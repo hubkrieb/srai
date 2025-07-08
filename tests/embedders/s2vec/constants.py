b5735c2ddfae86aa280e1ac5b29021eb42ffac7f
hubkrieb <66721036+hubkrieb@users.noreply.github.com>
2025-07-09 01:21:05 +0200

    wip: first s2vec tests

---
+++ tests/embedders/s2vec/constants.py
@@ -0,0 +1,29 @@
+"""Constants for s2vec tests."""
+
+from srai.loaders.osm_loaders.filters import HEX2VEC_FILTER
+
+TRAINER_KWARGS = {"max_epochs": 1, "accelerator": "cpu", "deterministic": True}
+
+PREDEFINED_TEST_CASES = [
+    {
+        "test_case_name": "wro_s2_12",
+        "geocoding_name": "Wrocław, Poland",
+        # "root_region_index": "891e2040887ffff",
+        "seed": 42,
+        "tags": {
+            t: HEX2VEC_FILTER[t]
+            for t in [
+                "building",
+                "amenity",
+            ]
+        },
+        "img_res": 12,
+        "patch_res": 16,
+        "num_heads": 2,
+        "encoder_layers": 6,
+        "decoder_layers": 2,
+        "embedding_dim": 256,
+        "decoder_dim": 128,
+        "mask_ratio": 0.75,
+    },
+]
