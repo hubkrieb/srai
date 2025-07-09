348f55f8ceb1699d5cc630318fe6a57df29143fd
hubkrieb <66721036+hubkrieb@users.noreply.github.com>
2025-07-09 20:54:04 +0200

    wip: add test_files and update constants

--- tests/embedders/s2vec/constants.py
+++ tests/embedders/s2vec/constants.py
@@ -6,7 +6,7 @@ TRAINER_KWARGS = {"max_epochs": 1, "accelerator": "cpu", "deterministic": True}
 
 PREDEFINED_TEST_CASES = [
     {
-        "test_case_name": "wro_s2_12",
+        "test_case_name": "wro_s2_14",
         "geocoding_name": "Wrocław, Poland",
         # "root_region_index": "891e2040887ffff",
         "seed": 42,
@@ -17,8 +17,8 @@ PREDEFINED_TEST_CASES = [
                 "amenity",
             ]
         },
-        "img_res": 12,
-        "patch_res": 16,
+        "img_res": 14,
+        "patch_res": 18,
         "num_heads": 2,
         "encoder_layers": 6,
         "decoder_layers": 2,
